9e9921b80683c8f8d245f0cf3bba2f5c3254a8c4
Wenderson <wps713@gmail.com>
2019-11-12 18:20:28 -0300

    Add product Amount in button

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -9,6 +9,8 @@ import {
   ProductTitle,
   ProductPrice,
   AddButton,
+  ProductAmount,
+  ProductAmountText,
   AddButonText,
 } from './styles';
 
@@ -40,6 +42,10 @@ export default class Main extends Component {
               <ProductTitle>{item.title}</ProductTitle>
               <ProductPrice>{item.price}</ProductPrice>
               <AddButton>
+                <ProductAmount>
+                  <Icon name="add-shopping-cart" color="#FFF" size={20} />
+                  <ProductAmountText>1</ProductAmountText>
+                </ProductAmount>
                 <AddButonText>Adicionar</AddButonText>
               </AddButton>
             </Product>
