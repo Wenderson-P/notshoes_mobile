445cd58d4336735b70244c7145c4a0d18f0fd369
Wenderson <wps713@gmail.com>
2019-11-12 17:14:40 -0300

    Add initial Home page style

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View``;
+
+export const Product = styled.View``;
+export const ProductImage = styled.Image`
+  height: 200px;
+  width: 200px;
+`;
+
+export const ProductTitle = styled.View``;
+export const ProductPrice = styled.View``;
+export const AddButon = styled.View``;
+export const AddButonText = styled.View``;
