f1fbb0e293502022182bfb0c665af213f2eb6529
Wenderson <wps713@gmail.com>
2019-11-12 17:08:46 -0300

    Using routes in index

--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import './config/ReactotronConfig';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  welcome: {
-    fontSize: 20,
-  },
-});
-
-console.tron.warn('Hello World');
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Hello world</Text>
-    </View>
-  );
+  return <Routes />;
 }
