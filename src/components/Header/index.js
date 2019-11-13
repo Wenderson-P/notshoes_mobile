5022ae7d7cbae35fc3ca4b96d0c4fad5f9502203
Wenderson <wps713@gmail.com>
2019-11-13 10:33:32 -0300

    Add navigate to cart page when clicking on cart icon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Container, Logo, BasketButton, ItemAmount } from './styles';
 
-export default function components() {
+export default function components({ navigation }) {
   return (
     <Container>
       <Logo />
-      <BasketButton>
+      <BasketButton onPress={() => navigation.navigate('Cart')}>
         <Icon name="shopping-basket" color="#FFF" size={24} />
         <ItemAmount>2</ItemAmount>
       </BasketButton>
