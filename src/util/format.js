871da4463723e18029ad5da18d77c387cb28456c
Wenderson <wps713@gmail.com>
2019-11-12 21:15:37 -0300

    Changing intl to numeral library
    I was using intl in the web version, however, intl is not native in android.

--- src/util/format.js
+++ src/util/format.js
@@ -1,4 +1,6 @@
-export const { format: formatPrice } = new Intl.NumberFormat('pt-BR', {
-  style: 'currency',
-  currency: 'BRL',
-});
+import Numeral from 'numeral';
+import 'numeral/locales/pt-br';
+
+Numeral.locale('pt-br');
+
+export const formatPrice = price => Numeral(price).format('$0,0.00');
