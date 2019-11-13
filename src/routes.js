5ab043700ada0b92a62012b96c1e4febbb219f3c
Wenderson <wps713@gmail.com>
2019-11-13 09:45:49 -0300

    Add header component in routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,12 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+
 import Main from './pages/Main';
 import Cart from './pages/Cart';
+
+import Header from './components/Header';
+
 import colors from './styles/colors';
 
 const Routes = createAppContainer(
@@ -11,6 +16,9 @@ const Routes = createAppContainer(
       Cart,
     },
     {
+      defaultNavigationOptions: navigation => ({
+        header: <Header {...navigation} />,
+      }),
       cardStyle: {
         backgroundColor: colors.dark,
       },
