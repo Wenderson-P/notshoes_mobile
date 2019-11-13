a0f2504f98c4be9cecb735a9529746e24d07151d
Wenderson <wps713@gmail.com>
2019-11-13 16:36:59 -0300

    Add cart actions and reducers

---
+++ src/store/modules/cart/action.js
@@ -0,0 +1,20 @@
+export function addToCartRequest(id) {
+  return {
+    type: '@cart/ADD_REQUEST',
+    id,
+  };
+}
+
+export function addToCartSucess(product) {
+  return {
+    type: '@cart/ADD_SUCESS',
+    product,
+  };
+}
+
+export function removeFromCart(id) {
+  return {
+    type: '@cart/REMOVE',
+    id,
+  };
+}
