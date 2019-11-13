4dbfba14f8ff76b109ad83c2bb1af16f4f7fd069
Wenderson <wps713@gmail.com>
2019-11-13 10:53:46 -0300

    Add initial cart page style

---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View``;
+
+export const ProductContainer = styled.View``;
+export const ProductImage = styled.Image``;
+export const ProductDetails = styled.View``;
+export const ProductTitle = styled.Text``;
+export const ProductPrice = styled.Text``;
+export const RemoveButton = styled.TouchableOpacity``;
+export const ProductControls = styled.View``;
+export const ProductControlsButton = styled.TouchableOpacity``;
+export const ProductAmount = styled.Text``;
+export const ProductTotal = styled.Text``;
