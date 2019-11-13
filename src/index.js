4f5aaa814cc5ac2dfa77a55aad29f7de8f598561
Wenderson <wps713@gmail.com>
2019-11-13 16:13:14 -0300

    Setting store/redux in app.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './config/ReactotronConfig';
 import { StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
+import store from './store';
 import Routes from './routes';
 import NavigationService from './services/navigation';
 
 export default function App() {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar barStyle="light-content" />
       <Routes
         ref={navigatorRef => NavigationService.setNavigator(navigatorRef)}
       />
-    </>
+    </Provider>
   );
 }
