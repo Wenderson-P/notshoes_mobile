e3737e1059c363c4bb39e4f83b19fc712ea10af2
Wenderson <wps713@gmail.com>
2019-11-13 10:30:37 -0300

    Add main page style

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,14 +1,60 @@
 import styled from 'styled-components/native';
+import { darken } from 'polished';
+import colors from '../../styles/colors';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  background: ${colors.dark};
+`;
 
-export const Product = styled.View``;
+export const Product = styled.View`
+  width: 220px;
+  background: #fff;
+  padding: 10px;
+  margin: 15px;
+  border-radius: 4px;
+`;
+
 export const ProductImage = styled.Image`
   height: 200px;
   width: 200px;
 `;
 
-export const ProductTitle = styled.View``;
-export const ProductPrice = styled.View``;
-export const AddButon = styled.View``;
-export const AddButonText = styled.View``;
+export const ProductTitle = styled.Text`
+  color: ${colors.grayColor};
+  font-size: 16px;
+  line-height: 20px;
+  height: 40px;
+`;
+export const ProductPrice = styled.Text`
+  font-size: 21px;
+  font-weight: bold;
+  margin: 5px 0px 15px;
+`;
+export const AddButton = styled.TouchableOpacity`
+  background: ${colors.primary};
+  border-radius: 4px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const AddButonText = styled.Text`
+  flex: 1;
+  color: #fff;
+  font-weight: bold;
+  text-transform: uppercase;
+  text-align: center;
+`;
+
+export const ProductAmount = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 10px;
+  background: ${darken(0.06, colors.primary)};
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+`;
+
+export const ProductAmountText = styled.Text`
+  color: #fff;
+  margin: 0px 4px 0px 10px;
+`;
