e84330c19f54d8fd3e8d8b513b189b754f63acce
Wenderson <wps713@gmail.com>
2019-11-13 10:53:37 -0300

    Add initial cart page content

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,8 +1,43 @@
 import React from 'react';
-import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import {
+  Container,
+  ProductContainer,
+  ProductImage,
+  ProductDetails,
+  ProductTitle,
+  ProductPrice,
+  RemoveButton,
+  ProductControls,
+  ProductControlsButton,
+  ProductAmount,
+  ProductTotal,
+} from './styles';
 
 export default function Cart() {
-  return <View />;
+  return (
+    <Container>
+      <ProductContainer>
+        <ProductImage />
+        <ProductDetails>
+          <ProductTitle>Tênis</ProductTitle>
+          <ProductPrice>R$ 159,00</ProductPrice>
+          <RemoveButton>
+            <Icon />
+          </RemoveButton>
+        </ProductDetails>
+        <ProductControls>
+          <ProductControlsButton>
+            <Icon />
+          </ProductControlsButton>
+          <ProductAmount>2</ProductAmount>
+          <ProductControlsButton>
+            <Icon />
+          </ProductControlsButton>
+          <ProductTotal>R$ 318,00</ProductTotal>
+        </ProductControls>
+      </ProductContainer>
+    </Container>
+  );
 }
