5c57901634d2c5e978179b270aebb9c67d288bcf
Wenderson <wps713@gmail.com>
2019-11-13 10:30:58 -0300

    Add header style

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,3 +1,31 @@
-import styled from 'styled-components';
+import styled from 'styled-components/native';
+import colors from '../../styles/colors';
+import logo from '../../assets/images/logo.png';
 
-export const Container = styled.div``;
+export const Container = styled.View`
+  background: ${colors.dark};
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+export const Logo = styled.Image.attrs({
+  source: logo,
+  resizeMode: 'cover',
+})``;
+
+export const BasketButton = styled.TouchableOpacity``;
+
+export const ItemAmount = styled.Text`
+  color: #fff;
+  background: ${colors.primary};
+  border-radius: 10px;
+  padding: 2px;
+  font-size: 12px;
+  min-width: 18;
+  min-height: 18;
+  text-align: center;
+  position: absolute;
+  right: -8px;
+  top: -8px;
+`;
